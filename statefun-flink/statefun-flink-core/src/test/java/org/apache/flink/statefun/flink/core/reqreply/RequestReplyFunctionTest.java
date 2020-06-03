4bda1970da56860c8cea5faf39d93cf42276539e
Tzu-Li (Gordon) Tai <tzulitai@apache.org>
2020-06-03 13:13:30 +0800

    [FLINK-XXXXX] [core] Use PersistedRemoteFunctionValues in RequestReplyFunction
    This de-multiplexes state values in remote functions. As a side effect,
    it also lets each state be configured with its own state TTL duration.

--- statefun-flink/statefun-flink-core/src/test/java/org/apache/flink/statefun/flink/core/reqreply/RequestReplyFunctionTest.java
+++ statefun-flink/statefun-flink-core/src/test/java/org/apache/flink/statefun/flink/core/reqreply/RequestReplyFunctionTest.java
@@ -59,7 +59,9 @@ public class RequestReplyFunctionTest {
 
   private final FakeClient client = new FakeClient();
   private final FakeContext context = new FakeContext();
-  private final List<StateSpec> states = Collections.singletonList(new StateSpec("session"));
+  private final PersistedRemoteFunctionValues states =
+      PersistedRemoteFunctionValues.fromStateSpecs(
+          Collections.singletonList(new StateSpec("session")));
 
   private final RequestReplyFunction functionUnderTest =
       new RequestReplyFunction(states, 10, client);
