7349de70444791eaebc770e2f9a2dd769f9a1083
Tzu-Li (Gordon) Tai <tzulitai@apache.org>
2020-06-03 13:03:51 +0800

    [FLINK-XXXXX] [core] Introduce PersistedRemoteFunctionValues
    This class encapsulates registered state of a remote function.

---
+++ statefun-flink/statefun-flink-core/src/main/java/org/apache/flink/statefun/flink/core/reqreply/PersistedRemoteFunctionValues.java
@@ -0,0 +1,73 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.statefun.flink.core.reqreply;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiConsumer;
+import org.apache.flink.statefun.flink.core.httpfn.StateSpec;
+import org.apache.flink.statefun.sdk.annotations.Persisted;
+import org.apache.flink.statefun.sdk.state.Expiration;
+import org.apache.flink.statefun.sdk.state.PersistedValue;
+
+public final class PersistedRemoteFunctionValues {
+
+  @Persisted private final Map<String, PersistedValue<byte[]>> managedStates;
+
+  public static PersistedRemoteFunctionValues fromStateSpecs(List<StateSpec> stateSpecs) {
+    final Map<String, PersistedValue<byte[]>> managedStates = new HashMap<>(stateSpecs.size());
+    stateSpecs.forEach(
+        spec -> {
+          final String stateName = spec.name();
+          managedStates.put(
+              stateName,
+              PersistedValue.of(
+                  stateName,
+                  byte[].class,
+                  Expiration.expireAfterReadingOrWriting(spec.ttlDuration())));
+        });
+    return new PersistedRemoteFunctionValues(managedStates);
+  }
+
+  private PersistedRemoteFunctionValues(Map<String, PersistedValue<byte[]>> managedStates) {
+    this.managedStates = managedStates;
+  }
+
+  void forEach(BiConsumer<String, byte[]> consumer) {
+    managedStates.forEach((stateName, handle) -> consumer.accept(stateName, handle.get()));
+  }
+
+  void setValue(String stateName, byte[] value) {
+    getStateHandleOrThrow(stateName).set(value);
+  }
+
+  void clearValue(String stateName) {
+    getStateHandleOrThrow(stateName).clear();
+  }
+
+  private PersistedValue<byte[]> getStateHandleOrThrow(String stateName) {
+    final PersistedValue<byte[]> handle = managedStates.get(stateName);
+    if (handle == null) {
+      throw new IllegalStateException(
+          "Accessing a non-existing remote function state: " + stateName);
+    }
+    return handle;
+  }
+}
